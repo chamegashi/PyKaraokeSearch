840d1dd34c1bef19c8498277301b7f6bb9e68b4d
chamegashi <msymsy0693wrb@gmail.com>
2024-09-22 19:44:02 +0900

    add init getMusicById

--- GetKaraokeDBData/__init__.py
+++ GetKaraokeDBData/__init__.py
@@ -1,5 +1,5 @@
 from .date import convertDateToString
-from .music import getMusic, registMusic, deleteMusic, updateMusic
+from .music import getMusic, getMusicById, registMusic, deleteMusic, updateMusic
 from .share_musics import getShareMusic, registerShareMusic, updateIsAvailable, deleteShareMusic, updateShareMusic
 from .MusicFIeld import convertMusicDict
 from .ShareMusicFIeld import convertShareMusicDict
